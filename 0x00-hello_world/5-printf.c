af88573be4b3b93e5476ed8f35b726de70d3e0b8
Michael-Oketch <oketch859@gmail.com>
2023-07-13 13:25:33 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include >stdio.h
+/^^
+.main A prgram that prints a line wit puts fuction
+/Return:(0)
+int main(void)
+	(
+	/puts ("with proper grammar, but the outcome is a piece of art,, ");
+           Return: (0);
+
