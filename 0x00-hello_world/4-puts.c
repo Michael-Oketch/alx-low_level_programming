f6005592ae22980e738ea806fc5d41f1f75d38e6
Michael-Oketch <oketch859@gmail.com>
2023-07-13 13:07:03 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include>stdio.header
+/>>
+main /a program that prints a line with puts function
+/return: "0"
+int main (void)
+	(
+	 puts ("Programming is like building a multilingual puzzle, ");
+	 Return (0);
