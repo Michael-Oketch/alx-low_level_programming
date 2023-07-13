0528a496e802d250d74c615878b45cb30b313dc1
Michael-Oketch <oketch859@gmail.com>
2023-07-13 14:10:35 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main aprogram that print a line with size function
+ * return:(0)
+ * int main (0)
+ * (
+ * int main (void)
+ * (
+ *        char (a)
+ *        int b;
+ *        long c;
+ *        long long int d;
+ *        float f;
+ *
+ *        print f (Size of a char:%lu1 byte(s)/ (onsidelong)
+ *        sizeof(a));
+ *        int b(Size of an int:lu%4 byte(s)/(unsignedlong)
+ *        sizeof(b)
+ *        longc(Size of a long int:lu%4 byte(s)/(signedlog)
+ *        size c;
